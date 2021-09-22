c1455a1ef1c1a7a45399a50682cc8fe2e831a2c7
Ethan Bustad <ethan.bustad@liferay.com>
2021-09-22 10:12:37 -0700

    Refactor dose-response calculation: OO for easier dev and use

--- dose_response.py
+++ dose_response.py
@@ -2,26 +2,47 @@ import csv
 from scipy.optimize import curve_fit
 import warnings
 
-# pct_value = (f(x) - min) / (max - min)
-# f(x) = c + (d - c) / (1 + (x / e)**b)
-# therefore:
-# x = e * ((d - c) / (pct_value*(max - min) + min - c) - 1)**(1/b)
-def find_pct(pct_value, b, c, d, e, min_, max_):
-	return e * ((d - c) / (pct_value * (max_ - min_) + min_ - c) - 1)**(1/b)
-
-# returns tuple (the parameters of the 4-param log-logistic model): b, c, d, and e:
-def fit_dose_response_model(xs, ys):
-	with warnings.catch_warnings():
-		warnings.simplefilter("ignore", RuntimeWarning)
-		popt, pcov = curve_fit(log_logistic_model, xs, ys)
-	return popt
-
 # Ritz 2009, https://doi.org/10.1002/etc.7, Eq. 2
 # `xs` is a numpy array of x values; b, c, d, and e are model parameters:
 # relative slope at inflection point, lower asymptote, upper asymptote, inflection point (EC_50)
 # returns y values
 def log_logistic_model(xs, b, c, d, e):
 	return c + (d - c) / (1 + (xs / e)**b)
+
+class Model:
+	def __init__(self, xs, ys, E_0=100, E_max=None):
+		self.xs = xs
+		self.ys = ys
+		self.E_0 = E_0
+		self.E_max = E_max
+
+		model = lambda xs, b, c, e: log_logistic_model(xs, b, c, E_0, e)
+		with warnings.catch_warnings():
+			warnings.simplefilter("ignore", RuntimeWarning)
+			popt, pcov = curve_fit(model, self.xs, self.ys)
+		self.b, self.c, self.e = popt
+
+	# pct_value = (f(x) - min) / (max - min)
+	# f(x) = c + (d - c) / (1 + (x / e)**b)
+	# yields x
+	def find_pct(self, pct_value):
+		if pct_value == 0.5:
+			return self.e
+
+		b = self.b
+		c = self.c
+		d = self.E_0
+		e = self.e
+		max_ = self.E_0
+		min_ = self.get_absolute_E_max()
+
+		return e * ((d - c) / (pct_value * (max_ - min_) + min_ - c) - 1)**(1/b)
+
+	def get_absolute_E_max(self):
+		return self.E_max if self.E_max is not None else self.c
+
+	def get_drug_E_max(self):
+		return self.c
 
 #
 # main
@@ -35,13 +56,13 @@ if __name__ == '__main__':
 			xs.append(float(x))
 			ys.append(float(y))
 
-	b, c, d, e = fit_dose_response_model(xs, ys)
+	model = Model(xs, ys)
 
-	ic_90 = find_pct(0.1, b, c, d, e, c, d)
-	ic_75 = find_pct(0.25, b, c, d, e, c, d)
-	ic_50 = e
+	ic_90 = model.find_pct(0.1)
+	ic_75 = model.find_pct(0.25)
+	ic_50 = model.find_pct(0.5)
 
-	print('E_max:', c, 'score')
+	print('E_max:', model.get_absolute_E_max(), 'score')
 	print('IC_90:', ic_90, 'μM')
 	print('IC_75:', ic_75, 'μM')
 	print('IC_50:', ic_50, 'μM')
